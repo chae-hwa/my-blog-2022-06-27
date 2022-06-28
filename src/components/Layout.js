abca7c98e57b8e104f783cd9c41e0d8e2fdb9d08
chae-hwa <ihj0519@naver.com>
2022-06-28 15:59:00 +0900

    사이트 제목 꾸미기

---
+++ src/components/Layout.js
@@ -0,0 +1,58 @@
+import { Link, useStaticQuery, graphql } from "gatsby";
+import React from "react";
+
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from "./Layout.module.css";
+
+function Layout({ pageTitle, children }) {
+  const data = useStaticQuery(graphql`
+    query MyQuery1{
+      site{
+        siteMetadata{
+          title
+        
+        }
+      }
+    }
+  `);
+
+  return (
+    <div className={container}>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header>{data.site.siteMetadata.title}</header>
+
+
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+
+      <div>Copyright All Rights</div>
+    </div>
+  );
+}
+
+export default Layout;
